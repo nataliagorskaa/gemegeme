cc1785e6eee7b3799db05e393013704fa453c232
nataliagorskaa <ngdigitaldesign@outlook.com>
2021-03-28 15:33:10 +0100

    Responsive navigation menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,37 +1,51 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import { Avatar } from 'antd';
 
 const navLinks = [
     {
-        title: 'Home',
-        path: '/'
+      title: 'Home',
+      path: '/'
     },
     {
-        title: 'Blog',
-        path: '/blog'
+      title: 'Contact Us',
+      path: '/contact-us'
     },
     {
-        title: 'Contact Us',
-        path: '/contact-us'
+      title: 'Blog',
+      path: '/blog'
     },
+
     {
-        title: 'Login',
-        path: '/login'
-    }
-]
+      title: 'Login',
+      path: '/login'
+    },
+  ]
 
 export default function Navigation () {
-    return (
-    <nav className="site-navigation">
-         <span>GemeGeme</span>
-         <ul>
-              {  navLinks.map((link, index) => (
+    const [menuActive, setMenuActive] = useState(false)
 
-                   <li key={index}>
-                     <Link to={link.path}> {link.title}</Link>
-                   </li>
-               )) }
-         </ul>
-    </nav>)
+    return (
+    <nav className= "site-navigation" role="navigation">
+        <span className="menu-title">GEMEGEME</span>
+        <div className={ `menu-content-container ${menuActive && 'active'}` }>
+            <ul>
+            { navLinks.map((link, index) => (
+                <li key={index}>
+                    <Link to={link.path}>{link.title}</Link>
+                </li>
+                ))
+            }
+            </ul>
+            <span className="menu-avatar-container">
+                <Avatar size={50} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                <span className="menu-avatar-name">Me</span>
+            </span>
+        </div>
+        <i 
+            className="ion-ios-menu"
+            onClick={(ev) => setMenuActive(!menuActive)}
+        />
+    </nav>
+  )
 }
